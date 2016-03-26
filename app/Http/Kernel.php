f69dbe75ea048ed5f247faf589abcf1bcfe05110
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-03-26 18:20:24 +0600

    csrf position changed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
+//        \App\Http\Middleware\VerifyCsrfToken::class,
         /*
          * API middlewares...
          */
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
         'role' => Zizaco\Entrust\Middleware\EntrustRole::class,
         'permission' => Zizaco\Entrust\Middleware\EntrustPermission::class,
         'ability' => Zizaco\Entrust\Middleware\EntrustAbility::class,
+        'csrf' => App\Http\Middleware\VerifyCsrfToken::class, //csrf registered as middleware route to avoid oauth errors
         /*
          * API middleware routes...
          */
